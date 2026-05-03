ba467978354cff24d9f809b10e7f4e128e0be50f
Master-Hash <hash@hash.moe>
2026-05-03 15:06:42 +0800

    test?

--- SetTransparency.cs
+++ SetTransparency.cs
@@ -9,40 +9,76 @@ using Windows.Win32.UI.WindowsAndMessaging;
 namespace GlassIt
 {
     [JSModule]
-    public class SetTransParency
+    public class Transparency
     {
         [JSExport("setTransparency")]
         [SupportedOSPlatform("windows5.0")]
         public static bool SetTransparency(int pid, byte alpha)
         {
             var mainproc = Process.GetProcessById(pid);
-            return (from proc in Process.GetProcessesByName(mainproc.ProcessName)
-                    where !string.IsNullOrEmpty(proc.MainModule?.FileName) &&
-                          proc.MainModule.FileName == mainproc.MainModule?.FileName
-                    select proc.MainWindowHandle
-                into hMainWnd
-                    where hMainWnd != IntPtr.Zero
-                    select PInvoke.GetWindowThreadProcessId(new HWND(hMainWnd), out _)
-                into tid
-                    select PInvoke.EnumThreadWindows(tid, delegate (HWND hWnd, LPARAM lParam)
+            var executablePath = GetMainModuleFileName(mainproc);
+            if (string.IsNullOrEmpty(executablePath))
+            {
+                return false;
+            }
+
+            var matchedWindow = false;
+            var success = true;
+
+            foreach (var proc in Process.GetProcessesByName(mainproc.ProcessName))
+            {
+                using (proc)
+                {
+                    if (!string.Equals(GetMainModuleFileName(proc), executablePath, StringComparison.OrdinalIgnoreCase))
                     {
-                        if (!PInvoke.IsWindowVisible(hWnd))
-                        {
-                            return true;
-                        }
+                        continue;
+                    }
 
-                        var windowLong = PInvoke.GetWindowLongPtr(hWnd, WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE);
-                        PInvoke.SetWindowLongPtr(
-                            hWnd,
-                            WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE,
-                            windowLong | (nint)WINDOW_EX_STYLE.WS_EX_LAYERED);
+                    var hMainWnd = proc.MainWindowHandle;
+                    if (hMainWnd == IntPtr.Zero)
+                    {
+                        continue;
+                    }
 
-                        return PInvoke.SetLayeredWindowAttributes(
-                            hWnd,
-                            new COLORREF(0),
-                            alpha,
-                            LAYERED_WINDOW_ATTRIBUTES_FLAGS.LWA_ALPHA);
-                    }, default)).All(result => result);
+                    var tid = PInvoke.GetWindowThreadProcessId(new HWND(hMainWnd), out _);
+                    if (tid == 0)
+                    {
+                        success = false;
+                        continue;
+                    }
+
+                    matchedWindow = true;
+                    success &= PInvoke.EnumThreadWindows(tid, (hWnd, _) => SetWindowAlpha(hWnd, alpha), default);
+                }
+            }
+
+            return matchedWindow && success;
+        }
+
+        private static string? GetMainModuleFileName(Process process)
+        {
+            return process.MainModule?.FileName;
+        }
+
+        [SupportedOSPlatform("windows5.0")]
+        private static BOOL SetWindowAlpha(HWND hWnd, byte alpha)
+        {
+            if (!PInvoke.IsWindowVisible(hWnd))
+            {
+                return true;
+            }
+
+            var windowLong = PInvoke.GetWindowLongPtr(hWnd, WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE);
+            PInvoke.SetWindowLongPtr(
+                hWnd,
+                WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE,
+                windowLong | (nint)WINDOW_EX_STYLE.WS_EX_LAYERED);
+
+            return PInvoke.SetLayeredWindowAttributes(
+                hWnd,
+                new COLORREF(0),
+                alpha,
+                LAYERED_WINDOW_ATTRIBUTES_FLAGS.LWA_ALPHA);
         }
     }
 }
