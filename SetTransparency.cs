4e3fad76ed94c70188d619962f41540796ada0e2
Master-Hash <hash@hash.moe>
2026-05-04 16:13:09 +0800

    绝望的又一次尝试

--- SetTransparency.cs
+++ SetTransparency.cs
@@ -15,70 +15,36 @@ namespace GlassIt
         [SupportedOSPlatform("windows5.0")]
         public static bool SetTransparency(int pid, byte alpha)
         {
-            using var mainproc = Process.GetProcessById(pid);
-            var executablePath = GetMainModuleFileName(mainproc);
-            if (string.IsNullOrEmpty(executablePath))
-            {
-                return false;
-            }
-
-            var matchedWindow = false;
-            var success = true;
-
+            var mainproc = Process.GetProcessById(pid);
             foreach (var proc in Process.GetProcessesByName(mainproc.ProcessName))
             {
-                using (proc)
+                if (!string.IsNullOrEmpty(proc.MainModule?.FileName) &&
+                    proc.MainModule?.FileName != mainproc.MainModule?.FileName)
+                    continue;
+
+                var hMainWnd = (HWND)proc.MainWindowHandle;
+                if (hMainWnd == HWND.Null)
+                    continue;
+
+                var tid = PInvoke.GetWindowThreadProcessId(hMainWnd, out uint _);
+
+                var result = PInvoke.EnumThreadWindows(tid, (hWnd, _) =>
                 {
-                    if (proc is null) { continue; }
-                    if (!string.Equals(GetMainModuleFileName(proc), executablePath, StringComparison.OrdinalIgnoreCase))
-                    {
-                        continue;
-                    }
-                    var hMainWnd = proc.MainWindowHandle;
-                    if (hMainWnd == IntPtr.Zero)
-                    {
-                        continue;
-                    }
+                    if (!PInvoke.IsWindowVisible(hWnd))
+                        return true;
 
-                    var tid = PInvoke.GetWindowThreadProcessId(new HWND(hMainWnd), out _);
-                    if (tid == 0)
-                    {
-                        success = false;
-                        continue;
-                    }
+                    var exStyle = (WINDOW_EX_STYLE)PInvoke.GetWindowLongPtr(hWnd, WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE);
+                    PInvoke.SetWindowLongPtr(hWnd, WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE,
+                        (int)(exStyle | WINDOW_EX_STYLE.WS_EX_LAYERED));
+                    return PInvoke.SetLayeredWindowAttributes(hWnd, default, alpha,
+                        LAYERED_WINDOW_ATTRIBUTES_FLAGS.LWA_ALPHA);
+                }, default);
 
-                    matchedWindow = true;
-                    success &= PInvoke.EnumThreadWindows(tid, (hWnd, _) => SetWindowAlpha(hWnd, alpha), default);
-                }
+                if (!result)
+                    return false;
             }
 
-            return matchedWindow && success;
-        }
-
-        private static string? GetMainModuleFileName(Process process)
-        {
-            return process.MainModule?.FileName;
-        }
-
-        [SupportedOSPlatform("windows5.0")]
-        private static BOOL SetWindowAlpha(HWND hWnd, byte alpha)
-        {
-            if (!PInvoke.IsWindowVisible(hWnd))
-            {
-                return true;
-            }
-
-            var windowLong = PInvoke.GetWindowLongPtr(hWnd, WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE);
-            PInvoke.SetWindowLongPtr(
-                hWnd,
-                WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE,
-                windowLong | (nint)WINDOW_EX_STYLE.WS_EX_LAYERED);
-
-            return PInvoke.SetLayeredWindowAttributes(
-                hWnd,
-                new COLORREF(0),
-                alpha,
-                LAYERED_WINDOW_ATTRIBUTES_FLAGS.LWA_ALPHA);
+            return true;
         }
     }
 }
